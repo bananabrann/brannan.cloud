28bef69f0c5c160a3da9e8e3d84a7c9be1b4d6df
bananabrann <plbrannan95@gmail.com>
2023-03-31 17:01:40 -0500

    Add code comments

--- src/app.js
+++ src/app.js
@@ -1,44 +1,48 @@
+// Configuration and variable initialization.
 let windowHeight = window.innerHeight;
 let windowWidth = window.innerWidth;
-
-// const animationDistance = windowWidth + cloud11.offsetWidth
 const animationSteps = 600;
 const animationDuration = 6000;
 const animationInterval = animationDuration / animationSteps;
 
+// Store the positional data of the clouds.
 let positions = {
   clouds1: 0,
   clouds2: windowWidth - windowWidth / 2,
   clouds3: 0,
 };
 
+// Get the HTML elements of the clouds.
 const clouds1 = document.getElementsByClassName("clouds1")[0];
 const clouds2 = document.getElementById("clouds2");
 const clouds3 = document.getElementById("clouds3");
 
+// Do the work.
 setInterval(animate, animationInterval);
 
-function isOutOfBounds(element) {
-  const rect = element.getBoundingClientRect();
-  return rect.left < -windowWidth;
-}
-
+// Moves a given cloud based on the configurations, multiplied by a given
+// coeffecient.
 function moveCloud(cloudName, cloudElement, coeffecient) {
+  // Calculate the new position.
   positions[cloudName] -=
     ((windowWidth + cloudElement.offsetWidth) / animationSteps) * coeffecient;
+  // Update the element with the new position.
   cloudElement.style.left = positions[cloudName] + "px";
 
+  // If the given element has left the view of the window, set it to the right.
   if (positions[cloudName] < -cloudElement.offsetWidth) {
     positions[cloudName] = windowWidth;
   }
 }
 
+// High-level logic for the main animation frames.
 function animate() {
   moveCloud("clouds1", clouds1, 0.125);
   moveCloud("clouds2", clouds2, 0.028);
   moveCloud("clouds3", clouds3, 0.01);
 }
 
+// Readjust window variables if window is resized.
 addEventListener("resize", (event) => {
   windowHeight = window.innerHeight;
   windowWidth = window.innerWidth;

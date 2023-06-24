8b85b150c617567f85a43545582c4e71638528dd
bananabrann <plbrannan95@gmail.com>
2023-06-23 19:39:40 -0500

    Swap to SvelteKit (#4)
    * SvelteKit init
    
    * Simple site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<section>
+    <p>
+        <span>Hello!</span> This is brannan.cloud, the root domain for all my services, hosting everything
+        from the website you see here to the TV application in my grandma's living room.
+    </p>
+    <p>
+        I'm a full-time software developer, a prior U.S. Marine, and as is tradition of all people
+        that doing with computers, my family's tech support.
+    </p>
+    <p>
+        This site is a little ugly right now, but I'm working on it. I'm currently moving it over to SvelteKit to match the rest of my projects and to hopefully make it a little easier to maintain.
+    </p>
+</section>
+<section>
+    <p><a href="https://github.com/bananabrann" target="_blank">@bananabrann</a></p>
+    <p>Software Developer</p>
+    <br />
+
+    <small>Website and systems by me. MIT license, 2023.</small>
+    <small>Art assets by various artists. All rights reserved.</small>
+    <small>v1.0.1</small>
+</section>
+
+<style lang="scss">
+    section {
+        max-width: 600px;
+    }
+</style>

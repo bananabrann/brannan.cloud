10919f6445ebeb07d7e764da728b9aa8a369e2d1
bananabrann <plbrannan95@gmail.com>
2023-11-29 10:45:44 -0600

    Swap buttons to a tags

--- src/routes/pro/+page.svelte
+++ src/routes/pro/+page.svelte
@@ -1,25 +1,35 @@
 <script>
+  import ListBox from "svelte-material-icons/ListBox.svelte";
   import SealVariant from "svelte-material-icons/SealVariant.svelte";
-  import FileEye from "svelte-material-icons/FileEye.svelte";
-  import FileDownload from "svelte-material-icons/FileDownload.svelte";
+  import FileAccount from "svelte-material-icons/FileAccount.svelte";
   import GitHub from "svelte-material-icons/Github.svelte";
   import Discord from "svelte-material-icons/Discord.svelte";
-  import ListBox from "svelte-material-icons/ListBox.svelte";
 </script>
 
 <main>
   <h1>Documents & Links</h1>
 
-  <button><ListBox /> See Projects</button>
-  <button><SealVariant /> View ITIL Certificate</button>
-  <button><SealVariant /> View AZ-204 Certificate</button>
-  <button><FileEye /> View Resume </button>
-  <button><FileDownload /> Download Resume as PDF </button>
+  <a class="button" href="/works">
+    <ListBox /> See Projects
+  </a>
+  <a class="button" href="https://files.brannan.cloud/docs/itil4.pdf" target="_blank">
+    <SealVariant /> ITIL Certificate
+  </a>
+  <a class="button" href="https://files.brannan.cloud/docs/az-204.png" target="_blank">
+    <SealVariant /> AZ-204 Certificate
+  </a>
+  <a class="button" href="https://files.brannan.cloud/docs/resume.pdf" target="_blank">
+    <FileAccount /> Resume
+  </a>
 
   <h1>Socials</h1>
 
-  <button><GitHub /> Open GitHub</button>
-  <button><Discord /> Open Discord</button>
+  <a class="button" href="https://github.com/bananabrann" target="_blank">
+    <GitHub /> GitHub
+  </a>
+  <a class="button" href="https://discordapp.com/users/321317378862350346" target="_self">
+    <Discord /> Discord
+  </a>
 </main>
 
 <style>
@@ -32,7 +42,11 @@
     /* height: 100vh; */
   }
 
-  button {
-    width: 20rem;
+  a.button {
+    width: 16rem;
+
+    &:hover {
+      width: 17rem;
+    }
   }
 </style>

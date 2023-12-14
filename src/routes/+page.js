fe315542f0b7bfef8a6f7a11f45de66045e71635
bananabrann <plbrannan95@gmail.com>
2023-12-14 07:18:31 -0600

    Move bio lang code to .js file

---
+++ src/routes/+page.js
@@ -0,0 +1,23 @@
+// let currentLang = "en";
+
+// Changes the language setting of the site, saving to localStorage.
+// If some weird stuff happens, or it's the user's first time visiting the site, the
+// site will default to English.
+/*
+$: bio =
+  currentLang === "es"
+    ? "Yo soy ingeniero de software, tubista, y un exmarino de los Estados Unidos. A mí también estoy aprendiendo español, y siempre quiero usarlo cuando puedo."
+    : "I'm a software engineer, tuba player, and a prior U.S. Marine.";
+*/
+
+/*
+function changeLang(lang) {
+    if (lang === "es") {
+      currentLang = "es";
+    } else {
+      currentLang = "en";
+    }
+
+    localStorage.setItem("brannan.cloud-bioLang", currentLang);
+  }
+*/
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,15 +21,6 @@
    */
   export let data;
 
-  let currentLang = "en";
-
-  /*
-  $: bio =
-    currentLang === "es"
-      ? "Yo soy ingeniero de software, tubista, y un exmarino de los Estados Unidos. A mí también estoy aprendiendo español, y siempre quiero usarlo cuando puedo."
-      : "I'm a software engineer, tuba player, and a prior U.S. Marine.";
-  */
-
   onMount(() => {
     // If user has selected a language before, use that language.
     if (browser) {
@@ -38,21 +29,6 @@
       if (localStorageLanguage) currentLang = localStorageLanguage;
     }
   });
-
-  // Changes the language setting of the site, saving to localStorage.
-  // If some weird stuff happens, or it's the user's first time visiting the site, the
-  // site will default to English.
-  /*
-  function changeLang(lang) {
-    if (lang === "es") {
-      currentLang = "es";
-    } else {
-      currentLang = "en";
-    }
-
-    localStorage.setItem("brannan.cloud-bioLang", currentLang);
-  }
-  */
 </script>
 
 <main>

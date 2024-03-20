a091af8923a6a091a7b6b25eb0f80be7eca26c68
bananabrann <plbrannan95@gmail.com>
2024-03-20 17:15:29 -0500

    Basic flex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,8 +30,8 @@
   });
 </script>
 
-<div class="flex flex-col max-w-6xl m-auto p-8">
-  <!-- <div class="w-full sm:w-2/3">
+<div class="flex flex-col sm:flex-row max-w-6xl m-auto p-8">
+  <div class="w-full sm:w-1/2 md:w-2/3">
     <p>
       My name is Lee. I am a software developer in the USA making apps and systems Americans use
       around the world 🌐
@@ -39,87 +39,89 @@
       Most of my career has been web development with JavaScript and TypeScript, but I've dabbled in
       other tech, especially in my early career.
       <br />
-      The website is limbo right now, but I'm working on it. 
+      The website is limbo right now, but I'm working on it.
     </p>
-  </div> -->
+  </div>
 
-  <h2 class="text-3xl text-center my-8">Quick Links</h2>
-  <div class="flex mx-4 my-2 flex-col gap-5 flex-wrap justify-between sm:flex-row">
-    <div class=" bg-slate-800 bg-opacity-75 shadow-lg rounded-lg flex-auto basis-1/3 backdrop-blur-sm">
-      <div class="p-6">
-        <h3 class="text-2xl font-bold mb-2">Grandma's TV</h3>
-        {#await data.streamed.isTvOnline}
-          <WebStatusBadge status="loading" />
-        {:then isTvOnline}
-          <WebStatusBadge status={isTvOnline ? "up" : "down"} />
-        {:catch error}
-          <WebStatusBadge status="error" />
-        {/await}
-        <p class="text-sm">
-          TV application for my grandma, making it simple and clear for her to select what she wants
-          to watch. Also has a chatbot tailored to telling her what shows are on what streaming
-          service
-        </p>
-        <div class=" flex flex-col justify-start space-x-2 mt-4">
-          <a
-            href="https://tv.brannan.cloud"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded decoration max-w-36"
-          >
-            Go to TV
-          </a>
-          <a
-            href="https://github.com/bananabrann/tv.brannan.cloud"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="text-sm mt-2 underline"
-          >
-            View source
-          </a>
+  <div class="w-full sm:w-1/2 md:w-1/3">
+    <h2 class="text-3xl text-center my-8">Quick Links</h2>
+    <div class="flex mx-4 my-2 flex-col gap-5 flex-wrap justify-between sm:flex-row">
+      <div class=" bg-slate-800 bg-opacity-75 shadow-lg rounded-lg backdrop-blur-sm">
+        <div class="p-6">
+          <h3 class="text-2xl font-bold mb-2">Grandma's TV</h3>
+          {#await data.streamed.isTvOnline}
+            <WebStatusBadge status="loading" />
+          {:then isTvOnline}
+            <WebStatusBadge status={isTvOnline ? "up" : "down"} />
+          {:catch error}
+            <WebStatusBadge status="error" />
+          {/await}
+          <p class="text-sm">
+            TV application for my grandma, making it simple and clear for her to select what she
+            wants to watch. Also has a chatbot tailored to telling her what shows are on what
+            streaming service
+          </p>
+          <div class=" flex flex-col justify-start space-x-2 mt-4">
+            <a
+              href="https://tv.brannan.cloud"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded decoration max-w-36"
+            >
+              Go to TV
+            </a>
+            <a
+              href="https://github.com/bananabrann/tv.brannan.cloud"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-sm mt-2 underline"
+            >
+              View source
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div
-      class=" bg-slate-800 bg-opacity-75 shadow-lg rounded-lg overflow-hidden flex-auto basis-1/3 backdrop-blur-sm"
-    >
-      <div class="p-6">
-        <h3 class="text-2xl font-bold mb-2">ChatLibre</h3>
-        {#await data.streamed.isChatOnline}
-          <WebStatusBadge status="loading" />
-        {:then isTvOnline}
-          <WebStatusBadge status={isTvOnline ? "up" : "down"} />
-        {:catch error}
-          <WebStatusBadge status="error" />
-        {/await}
-        <p class="text-sm">
-          Enhanced ChatGPT clone for the Brannan family without any usage limitations. Only runs as
-          needed for cost savings. Need it turned on? Just ask me!
-        </p>
-        <div class=" flex flex-col justify-start space-x-2 mt-4">
-          <a
-            href="https://example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-36"
-          >
-            Visit
-          </a>
-          <a
-            href="https://github.com/bananabrann/chat.brannan.cloud"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="text-sm mt-2 underline"
-          >
-            View source
-          </a>
-          <a
-            href="https://docs.librechat.ai/"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="text-sm mt-2 underline">ChatLibre documentation</a
-          >
+      <div
+        class=" bg-slate-800 bg-opacity-75 shadow-lg rounded-lg overflow-hidden backdrop-blur-sm"
+      >
+        <div class="p-6">
+          <h3 class="text-2xl font-bold mb-2">ChatLibre</h3>
+          {#await data.streamed.isChatOnline}
+            <WebStatusBadge status="loading" />
+          {:then isTvOnline}
+            <WebStatusBadge status={isTvOnline ? "up" : "down"} />
+          {:catch error}
+            <WebStatusBadge status="error" />
+          {/await}
+          <p class="text-sm">
+            Enhanced ChatGPT clone for the Brannan family without any usage limitations. Only runs
+            as needed for cost savings. Need it turned on? Just ask me!
+          </p>
+          <div class=" flex flex-col justify-start space-x-2 mt-4">
+            <a
+              href="https://example.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-36"
+            >
+              Visit
+            </a>
+            <a
+              href="https://github.com/bananabrann/chat.brannan.cloud"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-sm mt-2 underline"
+            >
+              View source
+            </a>
+            <a
+              href="https://docs.librechat.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-sm mt-2 underline">ChatLibre documentation</a
+            >
+          </div>
         </div>
       </div>
     </div>

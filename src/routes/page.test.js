bd27e39da351b2acd8a3c1e0bf5814bb92b5ff32
bananabrann <plbrannan95@gmail.com>
2024-04-18 15:57:01 -0500

    Remove 404 test

--- src/routes/page.test.js
+++ src/routes/page.test.js
@@ -1,8 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { _isWebsiteOnline } from "./+page.server.js";
 import { page } from "./+page.js";
-const BASE_URL =
-	process.env.NODE_ENV !== "development" ? "http://localhost:5173" : "https://brannan.cloud";
 
 // NOTE - If WebStatusBadge ever becomes self-containing (see GitHub issue #44), the tests
 // should probably be moved to WebStatusBadge.test.js instead of here.
@@ -13,12 +11,5 @@ describe.concurrent("website status badges", () => {
 
 	test("returns false for an invalid website", async () => {
 		expect(await _isWebsiteOnline("https://google.com/404")).toBeFalsy();
-	});
-});
-
-describe.concurrent("404 error page", () => {
-	test("non-existent page returns a 404", async () => {
-		console.log("BASE_URL:", BASE_URL);
-		expect((await fetch(`${BASE_URL}/apple-pie`)).status).toBe(404);
 	});
 });

e0c1795c10ef7f97d3d9d2ab27d85dcada2756cf
bananabrann <plbrannan95@gmail.com>
2024-04-18 14:49:36 -0500

    Implement simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from "$app/stores";
+	import InternalScreamingPng from "$lib/assets/png/internal-screaming.png";
+</script>
+
+<div class="min-h-96 flex flex-col justify-center text-center mt-6 mb-32 mx-auto gap-4 w-5/6">
+	<img src={InternalScreamingPng} alt="" class="max-w-full mx-auto" />
+
+	<h1 class="text-7xl font-bold text-red-600">
+		{$page.status}
+	</h1>
+	<h2 class="font-bold text-red-600 text-4xl">
+		{$page.error.message}
+	</h2>
+</div>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-export function load({ params }) {
+export async function load({ params }) {
 	return {
 		streamed: {
 			isChatOnline: new Promise((fulfill) => {

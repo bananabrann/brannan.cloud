feb7b90ac77af5b508e6126a78ce03ea2de55740
bananabrann <plbrannan95@gmail.com>
2024-04-18 20:50:21 -0500

    Modify star and cloud boards to be "sticky" above footer

--- src/lib/components/ReadingPage.svelte
+++ src/lib/components/ReadingPage.svelte
@@ -1,5 +1,7 @@
-	<div class="flex justify-center">
-		<div class="bg-slate-100 bg-opacity-90 p-5 mx-3.5 md:p-6 lg:p-8 shadow-lg rounded-t-3xl max-w-2xl text-zinc-950 mt-8">
-			<slot></slot>
-		</div>
+<div class="flex justify-center">
+	<div
+		class="bg-slate-100 bg-opacity-90 p-5 mx-3.5 md:p-6 lg:p-8 shadow-lg rounded-t-3xl max-w-2xl text-zinc-950 mt-8"
+	>
+		<slot />
 	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import "../app.scss";
+	import { onMount } from "svelte";
 	import Lock from "svelte-material-icons/Lock.svelte";
 	import Clouds1 from "$lib/assets/svg/clouds1.svg.svelte";
 	import Clouds2 from "$lib/assets/svg/clouds2.svg.svelte";
@@ -7,6 +8,27 @@
 	import Stars from "$lib/assets/svg/stars.svg.svelte";
 	import OldEGA from "$lib/assets/png/old-ega.png";
 	import { version } from "$app/environment";
+
+	let cloudBoard;
+
+	onMount(() => {
+		const footer = document.querySelector("footer");
+		const footerHeight = footer.offsetHeight;
+
+		window.addEventListener("scroll", () => {
+			const scrollHeight = document.documentElement.scrollHeight;
+			const scrollTop = document.documentElement.scrollTop;
+			const windowHeight = window.innerHeight;
+
+			if (scrollHeight - scrollTop - windowHeight < footerHeight) {
+				cloudBoard.style.position = "absolute";
+				// cloudBoard.style.bottom = `${footerHeight}px`;
+			} else {
+				cloudBoard.style.position = "fixed";
+				cloudBoard.style.bottom = "0";
+			}
+		});
+	});
 </script>
 
 <head>
@@ -24,7 +46,7 @@
 		<Stars />
 	</div>
 
-	<div id="cloud-board">
+	<div id="cloud-board" bind:this={cloudBoard}>
 		<div class="cloud-row">
 			<span class="cloud cloud-slow">
 				<Clouds3 />
@@ -54,45 +76,55 @@
 	</div>
 </main>
 
-<footer class="bg-slate-800 flex flex-wrap p-4 max-w-2xl m-auto">
-	<div class="w-full sm:w-1/2 p-2">
-		<h4 class="font-thin text-gray-400">Index</h4>
-		<ul class="ml-8">
-			<li><a href="/">Home</a></li>
-			<li><a href="/works">Projects & Works</a></li>
-		</ul>
+<footer class="bg-slate-800 p-4 w-full z-10 relative">
+	<!--
+		NOTE - Inner container is needed so that the footer covers the star board. Without
+		this container, the footer's width technicaly doesn't expand the full width therefore
+		some stars are visible. Of course, if the content expanded with it, it'd look weird.
+		So here, the content itself lives within the footer whose width is 100% of the viewport.
+	-->
+	<div class="max-w-2xl m-auto flex flex-wrap">
+		<div class="w-full sm:w-1/2 p-2">
+			<h4 class="font-thin text-gray-400">Index</h4>
+			<ul class="ml-8">
+				<li><a href="/">Home</a></li>
+				<li><a href="/works">Projects & Works</a></li>
+			</ul>
 
-		<h4 class="font-thin text-gray-400">Sites</h4>
-		<ul class="ml-8">
-			<li><a href="https://tv.brannan.cloud">Grandma's TV</a></li>
-			<li><a href="http://chat.brannan.cloud">LibreChat</a></li>
-			<li class="text-gray-400">
-				<a href="https://files.brannan.cloud" class="flex flex-row"
-					><Lock class="mt-1 mr-1" /> Brannan File Share</a
-				>
-			</li>
-		</ul>
+			<h4 class="font-thin text-gray-400">Sites</h4>
+			<ul class="ml-8">
+				<li><a href="https://tv.brannan.cloud">Grandma's TV</a></li>
+				<li><a href="http://chat.brannan.cloud">LibreChat</a></li>
+				<li class="text-gray-400">
+					<a href="https://files.brannan.cloud" class="flex flex-row"
+						><Lock class="mt-1 mr-1" /> Brannan File Share</a
+					>
+				</li>
+			</ul>
 
-		<h4 class="font-thin text-gray-400">Socials</h4>
-		<ul class="ml-8">
-			<li><a href="https://github.com/bananabrann" target="_self">GitHub</a></li>
-			<li><a href="https://discordapp.com/users/321317378862350346" target="_self">Discord</a></li>
-			<li><a href="https://linkedin.com/in/pbrann" target="_self">LinkedIn</a></li>
-		</ul>
-	</div>
+			<h4 class="font-thin text-gray-400">Socials</h4>
+			<ul class="ml-8">
+				<li><a href="https://github.com/bananabrann" target="_self">GitHub</a></li>
+				<li>
+					<a href="https://discordapp.com/users/321317378862350346" target="_self">Discord</a>
+				</li>
+				<li><a href="https://linkedin.com/in/pbrann" target="_self">LinkedIn</a></li>
+			</ul>
+		</div>
 
-	<div class="w-full sm:w-1/2 p-2 text-center">
-		<p class="">
-			Website by me,
-			<a href="https://github.com/bananabrann/brannan.cloud/blob/main/LICENSE" target="_blank"
-				>MIT license, 2024.
-			</a><br />
-			Art and web assets by various artists, all rights reserved.
-		</p>
-		<small>Build {version}</small>
+		<div class="w-full sm:w-1/2 p-2 text-center">
+			<p class="">
+				Website by me,
+				<a href="https://github.com/bananabrann/brannan.cloud/blob/main/LICENSE" target="_blank"
+					>MIT license, 2024.
+				</a><br />
+				Art and web assets by various artists, all rights reserved.
+			</p>
+			<small>Build {version}</small>
 
-		<div class="m-auto text-white h-20 w-20 mt-4">
-			<img src={OldEGA} alt="" />
+			<div class="m-auto text-white h-20 w-20 mt-4">
+				<img src={OldEGA} alt="" />
+			</div>
 		</div>
 	</div>
 </footer>
@@ -111,10 +143,11 @@
 	}
 
 	#cloud-board {
-		position: relative;
+		position: fixed;
+		width: 100%;
 		bottom: 0;
 		z-index: -1;
-		height: auto;
+		// height: auto;
 
 		.cloud-row {
 			display: flex;
@@ -136,7 +169,7 @@
 		}
 
 		.cloud-medium {
-			animation: moveToLeft 125s linear infinite;
+			animation: moveToLeft 165s linear infinite;
 		}
 
 		.cloud-slow {
@@ -152,10 +185,10 @@
 	}
 
 	#star-board {
-		position: absolute;
+		position: fixed;
 		top: 0;
-		z-index: -2;
+		z-index: -10;
 		width: 100%;
-		height: 100%;
+		max-height: 100%;
 	}
 </style>

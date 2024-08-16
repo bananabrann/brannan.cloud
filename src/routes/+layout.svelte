d04871c24b198d5fa909587dc369865e66dfbb59
bananabrann <plbrannan95@gmail.com>
2024-08-16 09:36:17 -0500

    Move head to svelte:head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,13 +45,14 @@
 	}
 </script>
 
-<head>
+<svelte:head>
+	<title>The Brannan Cloud</title>
+	<meta name="description" content="Home site of the @bananabrann" />
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>brannan.cloud</title>
 	<link rel="stylesheet" href="https://use.typekit.net/xtr6jqc.css" />
-</head>
+</svelte:head>
 
 <main>
 	<slot />

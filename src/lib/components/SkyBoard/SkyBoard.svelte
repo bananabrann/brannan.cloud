9cdeb95b3961840df117f978bf8f9529f12caed4
bananabrann <plbrannan95@gmail.com>
2024-09-05 16:39:40 -0500

    Sticky clouds

--- src/lib/components/SkyBoard/SkyBoard.svelte
+++ src/lib/components/SkyBoard/SkyBoard.svelte
@@ -2,13 +2,45 @@
 	import CloudsForeground from "$lib/assets/png/clouds-foreground.png";
 	import CloudsMidground from "$lib/assets/png/clouds-midground.png";
 	import CloudsBackground from "$lib/assets/png/clouds-background-no-doodles.png";
+	import { onMount } from "svelte";
+
+	let cloudBoard: HTMLElement;
+
+	function calculateCloudBoardPosition(): void {
+		let footer: HTMLElement | null;
+		let footerHeight: number;
+
+		footer = document.querySelector("footer");
+		footerHeight = footer ? footer.offsetHeight : 0;
+
+		const scrollHeight = document.documentElement.scrollHeight;
+		const scrollTop = document.documentElement.scrollTop;
+		const windowHeight = window.innerHeight;
+
+		if (scrollHeight - scrollTop - windowHeight < footerHeight) {
+			cloudBoard.style.position = "absolute";
+		} else {
+			cloudBoard.style.position = "fixed";
+			cloudBoard.style.bottom = "0";
+		}
+	}
+
+	onMount(() => {
+		calculateCloudBoardPosition();
+
+		window.addEventListener("scroll", () => {
+			calculateCloudBoardPosition();
+		});
+
+		window.addEventListener("resize", () => {
+			calculateCloudBoardPosition();
+		});
+	});
 </script>
 
 <div class="sky-gradient-background min-h-[80vh] relative">
-	<slot />
-
 	<!-- NOTE - CSS is in <style> -->
-	<div id="cloud-board">
+	<div id="cloud-board" bind:this={cloudBoard}>
 		<div class="cloud-row" style="--duration: var(--speed-clouds-background);">
 			<img src={CloudsBackground} alt="Distant clouds in the background" />
 			<img src={CloudsBackground} alt="Distant clouds in the background" />
@@ -23,6 +55,10 @@
 			<img src={CloudsForeground} alt="Clouds in the foreground" />
 			<img src={CloudsForeground} alt="Clouds in the foreground" />
 		</div>
+	</div>
+
+	<div class="relative z-10">
+		<slot />
 	</div>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import Footer from "$lib/components/Footer/Footer.svelte";
 	import SkyBoard from "$lib/components/SkyBoard/SkyBoard.svelte";
+	import BananamanOnCloud from "$lib/assets/png/bananaman-on-cloud.png";
 </script>
 
 <SkyBoard>
 	<h1>Hi</h1>
+
+	<img src={BananamanOnCloud} alt="">
+	<img src={BananamanOnCloud} alt="">
 </SkyBoard>
 
 

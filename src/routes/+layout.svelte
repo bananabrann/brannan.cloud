f2de12054c32a9d80ed4331ecd09d35e458b2694
bananabrann <plbrannan95@gmail.com>
2024-09-05 13:23:12 -0500

    New 3.0.0 project init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<svelte:head>
+	<title>The Brannan Cloud</title>
+	<meta name="description" content="Home site of the @bananabrann" />
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
+
+<slot />

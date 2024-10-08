7ce3f6ec1430df7090706038670b47f4d1fd305c
bananabrann <plbrannan95@gmail.com>
2024-10-08 18:02:48 -0500

    Move service check to client, workaround to #87

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,11 +4,17 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
 	return {
+		/*
+		NOTE - Streaming is not currently supported for load functions. See 
+		https://github.com/bananabrann/brannan.cloud/issues/87 for more information.
+		*/
+		/*
 		webStatuses: {
 			tv: isWebsiteOnline("https://tv.brannan.cloud"),
 			files: isWebsiteOnline("https://files.brannan.cloud"),
 			chat: isWebsiteOnline("http://146.190.0.104"),
 		},
+		*/
 	};
 };
 
